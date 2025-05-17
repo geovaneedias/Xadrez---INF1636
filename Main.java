9b4733d8d3b3ffd24726465e6afb5698e2fd29dc
geovaneedias <geovaneedias@gmail.com>
2025-05-17 19:16:06 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import model.ModelFacade;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         ModelFacade v = ModelFacade.getInstance(); // Usando Singleton
+        System.out.println("Passei por aqui");
     }
 }
