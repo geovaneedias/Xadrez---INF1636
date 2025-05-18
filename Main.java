cbcf65ca1ff48ae6e7310186a108acf1c74c1962
geovaneedias <geovaneedias@gmail.com>
2025-05-18 16:43:20 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import model.ModelFacade;
 public class Main {
     public static void main(String[] args) {
         ModelFacade v = ModelFacade.getInstance(); // Usando Singleton
-        System.out.println("Passei por aqui");
+        System.out.println("Instancia Iniciada");
         
         // Testes de movimento com Tabuleiro
-        boolean movimentoValido = v.moverPeca("e2", "e3");
-        System.out.println("Movimento e2 -> e3: " + movimentoValido);
+        boolean movimentoValido = v.moverPeca("c2", "c3");
+        System.out.println("Movimento c2 -> c3: " + movimentoValido);
         v.imprimirTabuleiro();
+        
    
     }
 }
